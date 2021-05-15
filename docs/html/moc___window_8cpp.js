7e447d732552210241dafe92121acb82b4747781
David Robles <47421546+diabloget@users.noreply.github.com>
2021-05-15 09:19:23 -0600

    Doxygen

---
+++ docs/html/moc___window_8cpp.js
@@ -0,0 +1,5 @@
+var moc___window_8cpp =
+[
+    [ "qt_meta_stringdata_Window_t", "structqt__meta__stringdata___window__t.html", "structqt__meta__stringdata___window__t" ],
+    [ "QT_MOC_LITERAL", "moc___window_8cpp.html#a75bb9482d242cde0a06c9dbdc6b83abe", null ]
+];
---
+++ docs/html/structqt__meta__stringdata___window__t.js
@@ -0,0 +1,5 @@
+var structqt__meta__stringdata___window__t =
+[
+    [ "data", "structqt__meta__stringdata___window__t.html#a493b391bf3b1f6e52ef6c6e761495b41", null ],
+    [ "stringdata0", "structqt__meta__stringdata___window__t.html#a4663d0fb462b01df678f6a63bebf9d45", null ]
+];
